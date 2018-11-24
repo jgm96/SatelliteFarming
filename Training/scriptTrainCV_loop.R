a5cf8b1e2412dbfa8447a28abe867dcd4a636c85
jgm <juliangm96@gmail.com>
2018-11-24 22:02:51 +0100

    Added plotting

--- Training/scriptTrainCV_loop.R
+++ Training/scriptTrainCV_loop.R
@@ -4,6 +4,7 @@ library(psych)
 require(caret)
 require(RWeka)
 
+#Suppress warnings in output
 options(warn=-1)
 
 set.seed(1)
@@ -16,6 +17,15 @@ datos$Suelo[datos$Suelo=="Arenoso "]<-"Arenoso"
 datos$Suelo<-factor(datos$Suelo)
 est<-summary(datos)
 
+#String variables for outputs
+str_prediction <- "\n\n========= Prediction ========\n\n"
+str_separator.eq <- "======================================================\n"
+str_separator.ln <- "------------------------------------------------------\n"
+str_resultsObtention <- "RESULTS OBTENTION\n"
+str_fit <- "\n\n========= Fit ========\n\n"
+str_finalModel <- "\n\n========= Final Model ========\n\n"
+
+
 #Variables to use
 basicas<-c(8,12,16:18,23,56)
 basicasndvi<-c(8,12,16:18,23,56,65,66)
@@ -26,17 +36,21 @@ numerosNSEWndvi<-c(8,12,16:18,23,42:45,56,65,66)
 todo<-c(4,5,6,8,12,16:18,23,57:62,42:46,56)
 todondvi<-c(4,5,6,8,12,16:18,23,57:62,42:46,56,65,66)
 
-######################## SELECT VARIABLES TO TRAIN WITH #####################################
+######################## CONFIGURATION #####################################
 
+#Select variables to train with
 variables <- list(basicas, basicasndvi, volumenes, volumenesndvi,numerosNSEW, numerosNSEWndvi, todo, todondvi)
 
-####################### SELECT VARIABLE NAMES TO EXPORT RESULTS #############################
-
+#Enter variable names to export results
 variables_used <- c("basicas", "basicasndvi", "volumenes", "volumenesndvi", "numerosNSEW", "numerosNSEWndvi", "todo", "todondvi")
 
-###################### SELECT METHODS TO TRAIN WITH ##################################
+#Select methods to train with
+#methods.used <- c("M5","knn","svmRadial","cubist","gbm","bayesglm", "rf")
+methods.used <- c("rf")
 
-methods.used <- c("M5","knn","svmRadial","cubist","gbm","bayesglm")
+#Indicate whether plot graphs
+plotting.fit <- "Yes"
+plotting.model <- "Yes"
 
 #Remove NA values
 prediction <- na.omit(datos)
@@ -80,24 +94,66 @@ for(selectedMethod in methods.used){
   }
 }
 
-cat("======================================================\n")
-cat("RESULTS OBTENTION\n")
-cat("======================================================\n")
+############### RESULTS OBTENTION ################
+
+cat(str_separator.eq)
+cat(str_resultsObtention)
+cat(str_separator.eq)
+
 
 for(selectedMethod in methods.used){
   index = 1
   cat("Selected method: ", selectedMethod, "\n")
   for(variable in variables){
+    
+    #Training, capturing output to maintain the console clean for methods such as gbm.
     garbage <- capture.output(fit <- train(Cosecha ~., data=prediction.train[variable], method=selectedMethod,
                                  trControl = train_control))
+    
+    #Predict using fit and the selected variable
     prediction <- predict(fit,prediction.test[variable])
+    
+    #Model build
     model <- data.frame(obs = prediction.test[variable]$Cosecha, pred = prediction)
-    out <- capture.output(fit, summary(fit), fit$finalModel, defaultSummary(model))
-    outputFile <- paste0(parentPath, "/", selectedMethod, "/", variables_used[index],".txt")
-    write(out, file = outputFile, sep = " ")
-    cat("Results saved in: \"", outputFile, "\"","\n", sep = "")
+    
+    #Plotting fit if required
+    if(plotting.fit == "Yes"){
+      outputFile.image <- paste0(parentPath, "/", selectedMethod, "/", variables_used[index], "_fit", ".png")
+     
+      png(filename = outputFile.image)
+      capture.output(print(plot(fit)))
+      dev.off()
+      
+      cat("Fit plot image saved to: \"", outputFile.image, "\"","\n", sep = "")
+    }
+    
+    #Plotting model if required
+    if(plotting.model == "Yes"){
+      outputFile.image <- paste0(parentPath, "/", selectedMethod, "/", variables_used[index], "_model", ".png")
+
+      png(filename = outputFile.image)
+      plot(model)
+      dev.off()
+
+      cat("Model plot image saved to: \"", outputFile.image, "\"","\n", sep = "")
+    }
+    
+    #Build path to save results.
+    outputFile.text <- paste0(parentPath, "/", selectedMethod, "/", variables_used[index],".txt")
+    
+    #Capture output results.
+    out <- capture.output( cat(str_prediction), defaultSummary(model), cat(str_fit), fit, summary(fit), cat(str_finalModel), fit$finalModel)
+    
+    #Store results in a file.
+    write(out, file = outputFile.text, sep = " ")
+    
+    cat("Results saved to: \"", outputFile.text, "\"","\n", sep = "")
     index <- index +1
+    
+    cat(str_separator.ln)
   }
 }
 cat("Finished with success.")
+
+
 
